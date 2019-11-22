3539fa176de36cb5df16625ecf912e256281ecee
Ashish Kumar <ashishkumar6494@gmail.com>
2019-11-22 15:00:32 +0530

    Initial Commit

---
+++ WeatherUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherUp
+//
+//  Created by 121outsource on 22/11/19.
+//  Copyright © 2019 AshishKumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WeatherUpTests/WeatherUpTests.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherUpTests.swift
+//  WeatherUpTests
+//
+//  Created by 121outsource on 22/11/19.
+//  Copyright © 2019 AshishKumar. All rights reserved.
+//
+
+import XCTest
+@testable import WeatherUp
+
+class WeatherUpTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
